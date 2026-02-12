0c968c9d0cd2759c02d87df7535219eaf7704e0b
Gaurav bakare <gauravbakare376@gmail.com>
2026-02-12 11:46:09 +0530

    change log Lazy loading concept implemented.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,16 @@ import { About } from './components/about/about';
 
 export const routes: Routes = [
     
-    {path:'',redirectTo:'home',pathMatch:'full'},  // pathmatch gives if you want to give full path or not
-    {path:'home',component:Home},
-    {path:'about',component:About}
+    // {path:'',redirectTo:'home',pathMatch:'full'},  // pathmatch gives if you want to give full path or not
+    // {path:'home',component:Home},
+    // {path:'about',component:About}
+
+    { 
+        path:'',
+        loadComponent:()=>import('./home/home').then((c)=>c.Home)    //lazyloading 
+    },
+        { 
+        path:'about',
+        loadComponent:()=>import('./components/about/about').then((c)=>c.About)
+    }
 ];
